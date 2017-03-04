f6ba8bfb2a4f35e1e87277b6fbf9fef0c251aa7f
Axel <axelnowaczyk@gmail.com>
2017-03-04 21:56:52 +0100

    Design changes

--- Gym Tracker/ViewControllers/AddWorkoutViewController.swift
+++ Gym Tracker/ViewControllers/AddWorkoutViewController.swift
@@ -8,32 +8,125 @@
 
 import UIKit
 
-class AddWorkoutTableViewController: UITableViewController {
-
-    @IBAction func leftBarButtonItemWasPressed(_ sender: UIBarButtonItem) {
-        self.navigationController?.dismiss(animated: true, completion: nil)
+class AddWorkoutViewController: UIViewController {
+    
+    @IBOutlet var userPickerView: UIPickerView!
+    @IBOutlet var weightPickerView: UIPickerView!
+    @IBOutlet var exorcisePictureImageView: UIImageView!
+    @IBOutlet var previousWorkoutTableView: UITableView!
+    @IBOutlet var currentWorkoutTableView: UITableView!
+    @IBOutlet var repsTextField: UITextField!
+    @IBOutlet var weightTextField: UITextField!
+    
+    @IBAction func cancelButtonWasTouched(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func addButtonWasTouched(_ sender: UIButton) {
+        
+        guard   let reps = Int(repsTextField.text ?? ""),
+                let weight = Double(weightTextField.text ?? ""),
+                reps != 0 && weight != 0 else {
+                
+                    let alert = AlertUtil.createBasicAlert(with: "OOPS", message: "You need to provide both weight and rips number to add workout.")
+                    self.present(alert, animated: true, completion: nil)
+                    return
+        }
+
+//        guard let lastSession = lastSession else {
+//            return
+//        }
+//        
+//        sessionProvider.addSet(performig: reps, with: weight, in: lastSession, for: exorcise)
+    }
+    
+    
+    fileprivate enum PickerViewTag: Int {
+        case user = 1
+        case weight = 2
+    }
+
+    fileprivate enum TableViewTag: Int {
+        case previous = 1
+        case current = 2
+    }
+    
+    
+    var exorciseName: String?
+    var sessionManager: SessionManager?
+    let userProvider = UserProvider()
+    let exorciseRovider = ExorciseProvider()
+    let weightConverter = WeightConverter()
+    
+    var users: [User] = [] {
+        didSet {
+            userPickerView.reloadAllComponents()
+            reloadData()
+        }
+    }
+    
+    var user: User {
+        return users[userPickerView.selectedRow(inComponent: 0)]
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        users = userProvider.users
     }
     
+    fileprivate func reloadData() {
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        clearTextFields()
+        previousWorkoutTableView.reloadData()
+        currentWorkoutTableView.reloadData()
+        
     }
-    */
+    
+    private func clearTextFields() {
+        weightTextField.text = ""
+        repsTextField.text = ""
+    }
 
+}
+
+extension AddWorkoutViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        
+        guard let pickerViewTag = PickerViewTag(rawValue: pickerView.tag) else {
+            return 0
+        }
+        
+        switch pickerViewTag {
+        case .user:
+            return users.count
+        case .weight:
+            return weightConverter.numberOfWeighTypes
+        }
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        guard let pickerViewTag = PickerViewTag(rawValue: pickerView.tag) else {
+            return "Unknown"
+        }
+        
+        switch pickerViewTag {
+        case .user:
+            return users[row].name ?? "Unknown"
+        case .weight:
+            return weightConverter.getWeightType(at: row).description
+        }
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        reloadData()
+    }
 }
