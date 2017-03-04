584255a3617b45e5f20a608c841590909a812fde
Axel <axelnowaczyk@gmail.com>
2017-03-04 21:57:41 +0100

    session manager added to the project

---
+++ Gym Tracker/Models/SessionManager.swift
@@ -0,0 +1,52 @@
+//
+//  SessionManager.swift
+//  Gym Tracker
+//
+//  Created by Axel Nowaczyk on 04/03/2017.
+//  Copyright © 2017 Axel Nowaczyk. All rights reserved.
+//
+
+import Foundation
+
+class SessionManager: NSObject {
+    
+    fileprivate(set) var currentSession: Session?
+    fileprivate(set) var previousSession: Session?
+    fileprivate let sessionProvider = SessionProvider()
+    
+    init(user: User, exorcise: Exorcise) {
+        super.init()
+        
+        let lastSessions = sessionProvider.getLastSessions(numberOfSessions: 2, for: user, performing: exorcise)
+        initSessions(sessions: lastSessions, for: user)
+        
+    }
+    
+    private func initSessions(sessions: [Session], for user: User) {
+        
+        switch sessions.count {
+        case 2:
+            let storedSession = sessions[0]
+            if NSCalendar.current.isDateInToday(storedSession.date as! Date) {
+                currentSession = storedSession
+                previousSession = sessions[1]
+            } else {
+                currentSession = sessionProvider.storeSession(for: user)
+                previousSession = storedSession
+            }
+        case 1:
+            let storedSession = sessions[0]
+            if NSCalendar.current.isDateInToday(storedSession.date as! Date) {
+                currentSession = storedSession
+            } else {
+                currentSession = sessionProvider.storeSession(for: user)
+                previousSession = storedSession
+            }
+        case 0:
+            currentSession = sessionProvider.storeSession(for: user)
+        default: break
+        }
+        
+    }
+    
+}
